94b714e0f3700a23e8522be01e0c06b3a8be47d3
gogumai <gonzalo.gumai@gmail.com>
2017-11-15 10:06:16 -0300

    Basic non-functional input

---
+++ src/containers/Main/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainComponent from '../../components/Main';
+
+export default class Main extends React.Component {
+  render() {
+    return <MainComponent />;
+  }
+}
